ec935c1120af6df4b5ad4363fb321777235caaac
professor339 <65883904+professor339@users.noreply.github.com>
2021-09-28 20:24:41 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+
+
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q1"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a1"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q2"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a2"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q3"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a3"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q4"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a4"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q5"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a5"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q6"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a6"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q7"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a7"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q8"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a8"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q9"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a9"]; ?> <xmp></p></div><hr></xmp>
+<xmp><button type="button" class="collapsible"></xmp><?php echo $_POST["q10"]; ?><xmp></button><div class="content"><p></xmp>
+<?php echo $_POST["a10"]; ?> <xmp></p></div><hr></xmp>
+
+
+
+      
+</body>
+</html>
